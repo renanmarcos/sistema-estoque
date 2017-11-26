40d6eddadcefcdeba0d8eebe5da4268bb8307cf9
Renan Marcos Ferreira <renan.ferreira18@fatec.sp.gov.br>
2017-11-25 22:43:12 -0200

    Reestruturação da arquitetura do Banco de Dados.

--- SistemaContabilidade/frm_planoContas.vb
+++ SistemaContabilidade/frm_planoContas.vb
@@ -1,6 +1,6 @@
 ﻿Public Class frm_planoContas
     Dim sugestoes As New AutoCompleteStringCollection
-    Dim id_entrada As Integer
+    Dim id_entrada, id_saida As Integer
     Dim cnpj = "88.724.022/0001-45"
     Dim qtd, vunitario, vtotal As Double
     Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
@@ -117,14 +117,14 @@
                                  txt_qtd As TextBox, txt_vunit As TextBox, grupoConta As String,
                                  cmb_categoria As ComboBox)
 
-        If txt_nome.Text = Nothing Or txt_desc.Text = Nothing Or txt_qtd.Text = Nothing Or txt_vunit.Text = Nothing Then
-            MsgBox("Preencha todos os campos")
-        Else
-            If cmb_tipo.SelectedItem = "Compra" Then
+        If cmb_tipo.SelectedItem = "Compra" Then
+            If txt_nome.Text = Nothing Or txt_desc.Text = Nothing Or txt_qtd.Text = Nothing Or txt_vunit.Text = Nothing Then
+                MsgBox("Preencha todos os campos")
+            Else
                 sql = "SELECT * FROM tb_produtos WHERE cnpj='" & cnpj & "' AND nome='" & txt_nome.Text & "'"
                 rs = db.Execute(sql)
 
-                If rs.EOF = False Then
+                If Not rs.EOF Then
                     id_entrada = rs.Fields("id_entrada").Value
 
                     sql = "INSERT INTO tb_entrada VALUES(" & id_entrada & ", " & txt_qtd.Text & ", " &
@@ -156,28 +156,74 @@
 
                     MsgBox("Produto inserido com sucesso!")
                 End If
+            End If
+        Else
+            'MsgBox("Venda (em desenvolvimento)")
+            'Continuar aqui.
+            'É necessário dar 3 insert... Um em PEPS, outro em UEPS e outro em Custo Médio
+
+            '- Lógica:
+            ' 1 - Verificar na tb_produtos o id do produto digitado
+
+            ' 2 - Com o id do produto, fazer um laço para verificar e somar o campo "quantidade"
+            ' enquanto existir produto com aquele id na cláusula WHERE
+
+            ' 3 - Se a quantidade do produto for igual ou menor que zero, mostrar uma mensagem dizendo
+            ' que não se pode vender aquele produto.
+
+            ' 4 - Se a quantidade for válida, fazer a venda nos três métodos de estoque.
+
+            ' MÉTODO PEPS:
+            ' 5 - Verificar antes se a quantidade digitada é maior ou igual a quantidade existente em estoque.
+
+            ' 6 - Enquanto a quantidade digitada for diferente de 0, retirar o primeiro produto encontrado no
+            ' laço e descontar na variável de quantidade. Assim que retirar, fazer um insert no tb_saida_peps
+            ' com as informações da quantidade retirada.
+            If txt_nome.Text = Nothing Or txt_desc.Text = Nothing Or txt_qtd.Text = Nothing Then
+                MsgBox("Preencha todos os campos")
             Else
-                MsgBox("Venda (em desenvolvimento)")
-                'Continuar aqui.
-                'É necessário dar 3 insert... Um em PEPS, outro em UEPS e outro em Custo Médio
+                sql = "SELECT * FROM tb_produtos WHERE cnpj='" & cnpj & "' AND nome='" & txt_nome.Text & "'"
+                rs = db.Execute(sql)
 
-                '- Lógica:
-                ' 1 - Verificar na tb_produtos o id do produto digitado
+                If Not rs.EOF Then
+                    Dim qtdEntrada, qtdSaida, qtdTotal, qtdDigitada As Integer
 
-                ' 2 - Com o id do produto, fazer um laço para verificar e somar o campo "quantidade"
-                ' enquanto existir produto com aquele id na cláusula WHERE
+                    qtdDigitada = Integer.Parse(txt_qtd.Text)
+                    id_saida = rs.Fields("id_saida").Value
 
-                ' 3 - Se a quantidade do produto for igual ou menor que zero, mostrar uma mensagem dizendo
-                ' que não se pode vender aquele produto.
+                    sql = "SELECT sum(quantidade) FROM tb_entrada WHERE id_entrada=" & id_saida
+                    rs = db.Execute(sql)
+                    qtdEntrada = rs.Fields(0).Value
 
-                ' 4 - Se a quantidade for válida, fazer a venda nos três métodos de estoque.
+                    sql = "SELECT sum(quantidade) FROM tb_saida_media WHERE id_saida=" & id_saida
+                    rs = db.Execute(sql)
+                    qtdSaida = rs.Fields(0).Value
 
-                ' MÉTODO PEPS:
-                ' 5 - Verificar antes se a quantidade digitada é maior ou igual a quantidade existente em estoque.
+                    qtdTotal = qtdEntrada - qtdSaida
 
-                ' 6 - Enquanto a quantidade digitada for diferente de 0, retirar o primeiro produto encontrado no
-                ' laço e descontar na variável de quantidade. Assim que retirar, fazer um insert no tb_saida_peps
-                ' com as informações da quantidade retirada.
+                    If qtdDigitada > qtdTotal Or qtdDigitada <= 0 Then
+                        MsgBox("Você está tentando vender mais produtos do que o existente no estoque.")
+                        Exit Sub
+                    Else
+                        ' AQUI ESTA ERRADO, MUDAR. CONTINUAR DAQUI...
+                        sql = "SELECT * FROM tb_entrada WHERE id_entrada=" & id_saida
+                        rs = db.Execute(sql)
+
+                        Do While Not rs.EOF Or qtdDigitada = 0
+                            qtdDigitada -= qtdTotal
+
+                            sql = "INSERT INTO tb_saida_peps VALUES(" & id_saida & ", " & rs.Fields("quantidade").Value & "," &
+                                "" & rs.Fields("valor_unitario").Value & ", " & rs.Fields("valor_total").Value & ", '" & grupoConta & "', " &
+                                "'" & DateTime.Now.ToString("dd/MM/yyyy") & "', '" & DateTime.Now.ToString("HH:mm:00") & "')"
+                            db.Execute(sql)
+                            rs.MoveNext()
+                        Loop
+
+                        MsgBox("Vendido com sucesso!")
+                    End If
+                Else
+                    MsgBox("Você não pode vender um produto que não existe.")
+                End If
             End If
         End If
     End Sub
